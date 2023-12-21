60dc32b2c9da74ade5a5ecb711a0fc2eab9800aa
Darick <darick@revel.cloud>
2023-12-21 10:46:23 -0800

    link to the Authorization section instead

--- components/api.tsx
+++ components/api.tsx
@@ -62,7 +62,7 @@ type PermissionProps = {
 export const Permission = ({ name }: PermissionProps): JSX.Element => {
   return (
     <Callout emoji="🔑">
-      <a href="/rest/overview#generate-an-api-key">API key permission</a>{" "}
+      <a href="/rest/overview#authorization">API key permission</a>{" "}
       <code
         className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10"
         dir="ltr"

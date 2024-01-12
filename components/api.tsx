d3e1556a1f76ade5fe05bd990cce312654701be9
Darick <darick@revel.cloud>
2024-01-12 11:04:57 -0800

    bash syntax coloring

--- components/api.tsx
+++ components/api.tsx
@@ -39,9 +39,7 @@ export const Resource = ({
           </code>
         </pre>
       </div>
-      <p className="nx-mt-6 nx-leading-7 first:nx-mt-0">
-        <h4>Example</h4>
-      </p>
+      <p className="nx-mt-6 nx-leading-7 first:nx-mt-0">Example</p>
       <div className="nextra-code-block nx-relative nx-mt-6 first:nx-mt-0">
         <pre
           className="nx-bg-primary-700/5 nx-mb-4 nx-overflow-x-auto nx-rounded-xl nx-subpixel-antialiased dark:nx-bg-primary-300/10 nx-text-[.9em] contrast-more:nx-border contrast-more:nx-border-primary-900/20 contrast-more:nx-contrast-150 contrast-more:dark:nx-border-primary-100/40 nx-py-4"
@@ -49,19 +47,23 @@ export const Resource = ({
           data-theme="default"
         >
           <code
-            className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10"
+            className={
+              style.bash +
+              " nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10"
+            }
             dir="ltr"
             data-language="bash"
             data-theme="default"
           >
             <span className="line">
-              curl -X {method}{" "}
+              <span className={style.bashCommand}>curl</span> -X {method}{" "}
               'https://api.reflect-server.net/v1/apps/&#123;app-id&#125;{path}'
-              \
+              <span className={style.bashSlash}> \</span>
             </span>
             <span className="line">
               {"     "}
-              -H 'Authorization: Basic &#123;api-key&#125;' {body ? "\\" : ""}
+              -H 'Authorization: Basic &#123;api-key&#125;'{" "}
+              {body ? <span className={style.bashSlash}> \</span> : ""}
             </span>
             {body ? (
               <span className="line">
